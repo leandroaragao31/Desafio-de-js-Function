3c6d9d850f11cf49b8657a2ea7c2d6dcead1c835
leandroaragao31 <leandroaragao31@gmail.com>
2021-05-19 13:05:06 -0300

    realizando desafio de function, parametros de uma function e return

---
+++ script.js
@@ -0,0 +1,38 @@
+//Resposta 1
+
+function peaceMessenger(){
+    console.log("Que seu dia seja, calmo, tranquilo e iluminado");
+}
+peaceMessenger();
+
+//Resposta 2
+
+function Name(nome){
+    console.log(nome);
+}
+Name("Leandro");
+
+//Resoposta 3
+
+function aboutMe(dados){
+    console.log(dados);
+}
+aboutMe("Meu nome é Leandro");
+aboutMe("tenho 30 anos");
+aboutMe("Meus estilhos musicais preferidos são Rock, Samba e MPB");
+
+//Resposta 4
+
+function moreInformation(dados){
+    console.log(dados);
+}
+moreInformation("meu filme favorito é Soul");
+moreInformation("minha musica favorita é Spiders do System of a down");
+
+//Resposta 5
+
+function triple(number){
+    return number*3;
+}
+triple(10);
+console.log(triple(10));
